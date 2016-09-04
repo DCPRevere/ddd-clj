d07c56552607e914f3ccdbcaa41119324eb9d962
Daniel Revere <dcprevere@gmail.com>
2016-09-04 11:12:26 +0100

    Begin creation of commands and events.

--- src/ddd_clj/core.clj
+++ src/ddd_clj/core.clj
@@ -1,7 +1,23 @@
 (ns ddd-clj.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defprotocol Aggregate
+  (aggregate-behaviour [args])
+
+(defrecord Command [agg-id data])
+(defrecord OtherCommand [agg-id data])
+
+(defmethod Handle Command [command])
+(defmethod Handle OtherCommand [command])
+
+(defrecord Event [agg-id data])
+(defrecord OtherEvent [agg-id data])
+
+(defmethod apply-event Event [state event])
+(defmethod apply-event OtherEvent [state event])
+
+(defprotocol CommandHandler
+  (perform [command state]))
+
+(defmulti apply-event
+  (fn [state event] (class event)))
